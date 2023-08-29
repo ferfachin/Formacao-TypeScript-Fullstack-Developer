f6f680d5c91d66d5c2084fd37ee850e3bf793495
ferfachin <fernandofachinrm@gmail.com>
2023-08-29 10:16:24 -0400

    :sparkles: Variaveis e tipos

---
+++ Fundamentos-do-typescript/introducao-ao-typescript/variaveis-e-tipos/app.ts
@@ -0,0 +1,13 @@
+let b:string = 'b';
+let n:number = 2;
+let x:boolean = true
+
+// Any não é recomendado, mas existe e pode ser usado dependendo do caso
+let m:any = 2
+m = "Fernando"
+m = false
+
+// Vários tipos, nao recomendado
+
+let obj: string | boolean | number = 2
+obj = "Fernando"
